0511bbd1d8503e8745ca9ab7fe15a5fcd0ada34e
danish <danish25299@gmail.com>
2024-10-19 18:23:02 +0900

    updating dependencies for npm publish

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
@@ -9,21 +11,24 @@ const http_proxy_middleware_1 = require("http-proxy-middleware");
 const path_1 = __importDefault(require("path"));
 const cells_1 = require("./routes/cells");
 const serve = (port, filename, dir, useProxy) => {
-    const app = (0, express_1.default)();
-    app.use((0, cells_1.createCellsRouter)(filename, dir));
-    if (useProxy) {
-        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
-            target: "http://localhost:5173",
-            ws: true,
-            logger: console,
-        }));
-    }
-    else {
-        const packagePath = require.resolve("local-client/dist/index.html"); // figure out the file dir
-        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
-    }
-    return new Promise((resolve, reject) => {
-        app.listen(port, resolve).on("error", reject);
-    });
+  const app = (0, express_1.default)();
+  app.use((0, cells_1.createCellsRouter)(filename, dir));
+  if (useProxy) {
+    app.use(
+      (0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: "http://localhost:5173",
+        ws: true,
+        logger: console,
+      })
+    );
+  } else {
+    const packagePath = require.resolve(
+      "@nestjsnote/local-client/dist/index.html"
+    ); // figure out the file dir
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+  }
+  return new Promise((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
 };
 exports.serve = serve;
